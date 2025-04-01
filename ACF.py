8ccfcf96b412ebe3d9fe939b515ea903cbbc1aa9
Rahul Singh <rahulabtar@gmail.com>
2025-04-01 13:40:30 -0400

    Update ACF.py
    Working AutoCorrelationFunction

--- ACF.py
+++ ACF.py
@@ -1,53 +1,76 @@
-import math
 
-#"lag = sampling frequency / target frequency"
-#looks correct...
+import numpy as np
+import csv
+
+#Finds the Autocorrelation for a given lag 
 def ACF(buffer, lag):
     R = 0 
-    for n in range(0, len(buffer) - lag - 1):
-        R += (buffer[n] * buffer[n - lag])
-    R /= len(buffer)
+    if lag == 0:
+        R = np.sum(buffer * buffer) 
+    else:
+        R = np.sum(buffer[:-lag] * buffer[lag:])
     return R
 
-def getCorr(samps, fLow, fHigh, fs):
+#find the local maxima by looking to the right and left of a sample, makes a list of all observed maxima and returns it
+def findLocalMaxima(corrs):
+    size = len(corrs)
+    maxima = []
+    for lag in range(1, size - 1):
+        if (corrs[lag] >= corrs[lag - 1] and corrs[lag] >= corrs[lag + 1]): 
+            maxima.append(lag + 1)
+    return maxima
+
+#utilizes finding local maxima to calculate the distance between 
+def getFreqRahul(buffer,fs):
+    maxima = findLocalMaxima(buffer)
+    if (len(maxima) < 2): return 0
+    return fs / (maxima[1] - maxima[0])
+    
+#find the correlation values for the entire buffer, return a list of corrs corresponding to different lag values
+def getCorr(samps):
     corrs = []
     # for lag in range(round(fs / fHigh) - 1, round(fs / fLow)):
     for lag in range(0, len(samps)):
          corrs.append((ACF(samps, lag))) 
     return corrs
 
-# fs / "best lag" = suspected freq
-# best lag = suspected freq * fs
-def getFreq(corrs, fs, fHigh):
-    corrs = corrs[1:] #get rid of first sample because that will be best 
-    # freq = fs / (corrs.index(max(corrs)) + (fs / fHigh) + 1) 
-    freq = fs / (corrs.index(max(corrs)) + 1) 
+#calculates the frequency detected by find the lag that corresponds to distance between two ACF peaks
+def getFreq(corrs, fs):
+    corrs = corrs[1:] #get rid of first sample because lag of zero will lead to highest AC value
+    freq = fs / (corrs.index(max(corrs)) + 1) #maybe should be plus 2
     return freq
 
-#looks correct 
-# fs / f = samples per cycle of freq of interest
-# above times * number of desired cycles of freq of interest = numSamp
+#scales the data between -1 and 1
+def maxAbsoluteScaling(data):
+    data = [abs(element) for element in data]
+    xMax = max(data)
+    data = [element / xMax for element in data]
+    return data
 
+#Generates a buffer of pure Sin wave given a frequency and sampling frequency and a number of samples
 def genSin(f, fs, numSamp):
-    samps = []
-    for n in range(0,numSamp):
-        samps.append(math.sin(math.pi * 2 * (f / fs) * n))
-    return samps 
+    n = np.arange(0, numSamp)
+    samps = np.sin(2 * np.pi * (f / fs) * n)
+    return samps
 
-fLow = 1 #lower range of freq guess
-fHigh = 20 #high range of freq guess
+#Creates a loop that increases the sampling frequency and lists key values into csv 
+with open('80Hz.csv', 'w', newline = '') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    # Write the header row
+    csvwriter.writerow(['Sampling Frequency', 'True Frequency', 'Frequency Calculated', 'ACF Vals:'])
 
-for i in range(20, 100):
-    fn = 10
-    fs = i
-    numSamps = 30 #round(fs / fn * 3) #num of samples in sample buffer
-    samps = genSin(fn, fs, numSamps)
-    corrs = getCorr(samps, fLow , fHigh, fs)[1:]
-    print("fs = " + str(i) + " ||  Corr best lag: " + str(corrs[1:].index(max(corrs[1:])) + 1) + " || " + "ideal lag " + str(fs / fn) + " || " + "freq calculated " + str(getFreq(corrs, fs, fHigh)))
-
-fn = 1 #freq of genSin
-fs = 9 #samp frequency to sample fn at 
-numSamps = round(fs / fn * 3) #num of samples in sample buffer
+    fn = 82 #about the freq of low e string guitar fundamental
+    for fs in range(48000, 48010):
+        numSamps = round(fs / fn * 3) #generates 3 cycles of signal
+        samps = genSin(fn, fs, numSamps)
+        corrs = getCorr(samps)
+        #corrBestLag = corrs[1:].index(max(corrs[1:]))
+        #idealLag = fs / fn
+        #bestCorr = round(max(corrs[1:]),3)
+        freqCalc = getFreqRahul(corrs, fs)
+        roundedcorrs = [round(corr, 3) for corr in corrs]
+        #csvwriter.writerow([fs, corrBestLag, idealLag, bestCorr, freqCalc] + roundedcorrs)
+        csvwriter.writerow([fs, fn, freqCalc] + roundedcorrs)
 
 
 
